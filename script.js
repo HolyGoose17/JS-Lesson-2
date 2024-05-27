47878dbf64a5cd3ba66bbfaf5a31ad49ef9090b8
HolyGoose17 <158603194+HolyGoose17@users.noreply.github.com>
2024-05-27 22:33:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+// // const carName = 'Audi' = ;
+
+// // let maxCarSpeed = '329 km/h';
+
+// // let carOwner = 'Pavel';
+
+// // alert(carName = 'Audi');
+
+// // alert(maxCarSpeed = '329 km/h');
+
+// // alert(carOwner = 'Pavel');
+
+// // операторы
+
+// // = - присвоить
+// // + 
+// // - 
+// // *
+// // / 
+// // ** - возведение в степень
+// // % - остаток от деления (модуль)
+// // ++ - инкремент => увеличить на 1
+// // ++2
+// // 2++
+// // -- - декремент = уменьшить на 1
+// // --2
+// // 2--
+
+// let a = 10;
+// let b = 4;
+
+// const res_1 = a + b;
+// const res_2 = a - b;
+// const res_3 = a * b;
+// const res_4 = a ** b;
+// const res_5 = a / b;
+// const res_6 = a % b;
+
+// console.log('+:', res_1);
+// console.log('-:', res_2);
+// console.log('*:', res_3);
+// console.log('**:', res_4);
+// console.log('/:', res_5);
+// console.log('%:', res_6);
+
+// const c = 5;
+// // делить можно
+// console.log(c / 0);
+// // Nan => not a number
+// // console.log(dklugj / 0);
+// console.log(0 / 0);
+
+// // - конкатенация строк
+// console.log('Hello' + 'World');
+// console.log('Hello' + 2); // - если один из операндов строка - получим строку
+
+// // - операторы присваивания
+
+// let x = 4;
+
+// x += 2; // x = x  +2;
+// x -= 2; // x = x  -2;
+// x *= 2; // x = x  *2;
+// x /= 2; // x = x / 2;
+// x **= 2; // x = x ** 2;
+// x %= 2; // x = x % 2;
+
+// console.log(x)
+
+// операторы сравнения => Boolean => true или false
+// == - 
+// === - равное и значение и тип
+
+// console.log('==:', '2' == 2);
+// console.log('===:', '2' === 2);
+
+// !=
+// !== - не равное значение или ге равный тип
+
+// console.log('!=:', 2 != 2);
+// console.log('!==:', 2 !== 2);
+
+// >
+// <
+// >=
+// <==
+// ?
+
+// console.log(4 > 6);
+// console.log(4 < 6);
+// console.log(4 >= 4);
+// console.log(4 <= 2);
+
+// console.log('3' > '25');
+// console.log('31' > '25');
+// console.log('312' > '251');
+
+// console.log('H' === 'h');
+
+console.log(null == undefined);
